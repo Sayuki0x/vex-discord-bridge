87d767b80a2f4774ca578cbbf1ec244fa6dc42ff
ExtraHash <ExtraHash@protonmail.com>
2021-01-20 03:58:34 -0700

    update for nuvex

--- src/index.ts
+++ src/index.ts
@@ -1,166 +1,178 @@
+import { sleep } from "@extrahash/sleep";
+import { Client as VexClient, IFile, IUser } from "@vex-chat/libvex";
 import ax from "axios";
 import { Client as DiscordClient, Message } from "discord.js";
+import FileType from "file-type";
 import fs from "fs";
-import { Client as VexClient, KeyRing } from "libvex";
+
 import { loadEnv } from "./utils/loadEnv";
 
 if (!fs.existsSync("./emojis.json")) {
-  fs.writeFileSync("./emojis.json", "{}", { flag: "wx" });
+    fs.writeFileSync("./emojis.json", "{}", { flag: "wx" });
 }
 
 const emojiList = JSON.parse(
-  fs.readFileSync("./emojis.json", { encoding: "utf8" })
+    fs.readFileSync("./emojis.json", { encoding: "utf8" })
 );
 
 loadEnv();
+export const {
+    VEX_CHANNEL_ID,
+    PK,
+    PW,
+    DISCORD_TOKEN,
+    DISCORD_SERVER_ID,
+    DISCORD_CHANNEL_ID,
+} = process.env;
 
-const keyring = new KeyRing("./keys");
-const vexClient = new VexClient(process.env.VEX_SERVER!, keyring, null, true);
-const username = "BridgeBot";
+main();
 
-const markdownImageRegex = /!\[.*?\]\((.*?)\)/g;
+async function main() {
+    const vexClient = await VexClient.create(PK!);
+    const username = "BridgeBot";
 
-let guildMember: any;
+    const markdownImageRegex = /!\[.*?\]\((.*?)\)/g;
 
-vexClient.on("ready", async () => {
-  await vexClient.register();
-  await vexClient.auth();
-});
+    let guildMember: any;
 
-vexClient.on("authed", async () => {
-  await vexClient.channels.join(process.env.VEX_CHANNEL_ID!);
-  await vexClient.users.update({
-    color: "hsl(0, 0%, 71%)",
-    userID: vexClient.user?.userID,
-  } as any);
-});
+    const userRecords: Record<string, IUser> = {};
 
-function getURLFromMarkdown(markdown: string) {
-  const url = markdown.split("(")[1];
-  return url.slice(0, url.length - 1);
+    const err2 = await vexClient.login(username, PW!);
+    if (err2) {
+        console.error(err2.toString());
+    }
+
+    console.log(vexClient.me.user());
+
+    await vexClient.connect();
+
+    function getURLFromMarkdown(markdown: string) {
+        const url = markdown.split("(")[1];
+        return url.slice(0, url.length - 1);
+    }
+
+    vexClient.on("disconnect", () => {
+        console.log("The vex client disconnected.");
+        process.exit(1);
+    });
+
+    vexClient.on("message", async (message) => {
+        const channel = discordClient.channels.cache.get(
+            process.env.DISCORD_CHANNEL_ID!
+        );
+        if (!channel) {
+            return;
+        }
+
+        if (!userRecords[message.authorID]) {
+            const [user, err] = await vexClient.users.retrieve(
+                message.authorID
+            );
+            if (err) {
+                console.error(err);
+                return;
+            }
+            userRecords[message.authorID] = user!;
+        }
+
+        if (message.authorID !== vexClient.me.user().userID) {
+            if (channel) {
+                (channel as any).send(
+                    "**" +
+                        userRecords[message.authorID].username +
+                        "**: " +
+                        message.message
+                );
+            }
+        }
+    });
+
+    const discordClient: DiscordClient = new DiscordClient();
+
+    discordClient.login(DISCORD_TOKEN);
+
+    discordClient.on("disconnect", () => {
+        console.log("The discord client disconnected.");
+        process.exit(1);
+    });
+
+    discordClient.on("ready", async () => {
+        console.log(`Logged in as ${discordClient.user!.tag}!`);
+
+        await discordClient.user?.setStatus("invisible");
+
+        const guild: any = await discordClient.guilds.resolve(
+            DISCORD_SERVER_ID!
+        );
+        guildMember = await guild.members.resolve(
+            (discordClient as any).user.id
+        );
+    });
+
+    const emojiRegex = /<a?:\S+:\d{18}>/g;
+
+    const getEmojiID = (emojiString: string) => {
+        return [
+            emojiString.split(":")[1],
+            emojiString
+                .split(":")[2]
+                .slice(0, emojiString.split(":")[2].length - 1),
+        ];
+    };
+
+    discordClient.on("message", async (msg: Message) => {
+        if (msg.channel.id === process.env.DISCORD_CHANNEL_ID) {
+            if (msg.author.id !== process.env.DISCORD_USER_ID) {
+                // const emojiMatches = msg.content.match(emojiRegex);
+
+                // if (emojiMatches) {
+                //   for (const emojiString of emojiMatches) {
+                //     const [emojiName, emojiID] = getEmojiID(emojiString);
+                //     const res = await ax.get(
+                //         `https://cdn.discordapp.com/emojis/${emojiID}`,
+                //         { responseType: "arraybuffer" }
+                //       );
+                //       const [fileInfo, key] = await vexClient.files.create(
+                //         res.data
+                //       );
+                //       const type = await FileType.fromBuffer(res.data);
+                //       await vexClient.messages.group(VEX_CHANNEL_ID!, fileToString(emojiName, fileInfo, key, type?.mime || "image"))
+                //   }
+                // }
+
+                // if (msg.attachments.first()) {
+                //     const name = msg.attachments.first()?.name;
+                //     const url = msg.attachments.first()?.url;
+
+                //     const res = await ax.get(url!, {
+                //         responseType: "arraybuffer",
+                //     });
+                //     const [details, key] = await vexClient.files.create(
+                //         res.data
+                //     );
+
+                //     const attachmentType = await FileType.fromBuffer(res.data);
+                //     // markdown formatted link
+                //     vexClient.messages.group(
+                //         VEX_CHANNEL_ID!,
+                //         fileToString(
+                //             name || "file",
+                //             details,
+                //             key,
+                //             attachmentType?.mime || "unknown"
+                //         )
+                //     );
+                // }
+
+                vexClient.messages.group(
+                    VEX_CHANNEL_ID!,
+                    `${msg.author.username}: ${msg.content}`
+                );
+            }
+        }
+    });
 }
 
-vexClient.on("disconnect", (code) => {
-  console.log("The vex client disconnected with close code " + code.toString());
-  process.exit(1);
-});
-
-vexClient.on("dead_ping", () => {
-  console.log("The vex ping message between us and the server is dead.");
-  process.exit(1);
-});
-
-vexClient.on("message", async (message) => {
-  if (message.message.match(markdownImageRegex)) {
-    const matches = message.message.match(markdownImageRegex);
-    if (matches) {
-      message.message = message.message.replace(
-        matches[0],
-        getURLFromMarkdown(matches[0])
-      );
-    }
-  }
-
-  const channel = discordClient.channels.cache.get(
-    process.env.DISCORD_CHANNEL_ID!
-  );
-  if (!channel) {
-    return;
-  }
-
-  if (message.userID !== vexClient.user?.userID) {
-    if (channel) {
-      await (channel as any).send(
-        "**" + message.username + "**: " + message.message
-      );
-    }
-  }
-});
-
-const discordClient: DiscordClient = new DiscordClient();
-
-discordClient.login(process.env.DISCORD_TOKEN);
-
-discordClient.on("disconnect", () => {
-  console.log("The discord client disconnected.");
-  process.exit(1);
-});
-
-discordClient.on("ready", async () => {
-  console.log(`Logged in as ${discordClient.user!.tag}!`);
-
-  await discordClient.user?.setStatus("invisible");
-
-  const guild: any = await discordClient.guilds.resolve("579913226129637376");
-  guildMember = await guild.members.resolve((discordClient as any).user.id);
-});
-
-const emojiRegex = /<a?:\S+:\d{18}>/g;
-
-const getEmojiID = (emojiString: string) => {
-  return [
-    emojiString.split(":")[1],
-    emojiString.split(":")[2].slice(0, emojiString.split(":")[2].length - 1),
-  ];
+const fileToString = (name: string, file: IFile, key: string, type: string) => {
+    return `{{${name}:${file.fileID}:${key}:${type}}}`;
 };
-
-discordClient.on("message", async (msg: Message) => {
-  if (msg.channel.id === process.env.DISCORD_CHANNEL_ID) {
-    let attachment = "";
-    if (msg.author.id !== process.env.DISCORD_USER_ID) {
-      const emojiMatches = msg.content.match(emojiRegex);
-
-      if (emojiMatches) {
-        for (const emojiString of emojiMatches) {
-          const [emojiName, emojiID] = getEmojiID(emojiString);
-
-          if (!emojiList[emojiName]) {
-            const res = await ax.get(
-              `https://cdn.discordapp.com/emojis/${emojiID}`,
-              { responseType: "arraybuffer" }
-            );
-            const fileInfo = await vexClient.files.create(
-              res.data,
-              emojiName,
-              process.env.VEX_CHANNEL_ID!
-            );
-
-            emojiList[emojiName] = fileInfo.url;
-            fs.writeFileSync(
-              "./emojis.json",
-              JSON.stringify(emojiList, Object.keys(emojiList).sort(), 4)
-            );
-            console.log("Saved new emoji " + emojiName + " at " + fileInfo.url);
-            const emojiFile = `![emoji-${emojiName}](${fileInfo.url})`;
-            msg.content = msg.content.replace(emojiString, emojiFile);
-          } else {
-            console.log("Found " + emojiName + " in cache.");
-            const emojiFile = `![emoji-${emojiName}](${emojiList[emojiName]})`;
-            msg.content = msg.content.replace(emojiString, emojiFile);
-          }
-        }
-      }
-
-      if (msg.attachments.first()) {
-        const name = msg.attachments.first()?.name;
-        const url = msg.attachments.first()?.url;
-
-        const res = await ax.get(url!, { responseType: "arraybuffer" });
-        const fileInfo = await vexClient.files.create(
-          res.data,
-          name || "untitled",
-          process.env.VEX_CHANNEL_ID!
-        );
-
-        // markdown formatted link
-        attachment = `![${name || fileInfo.url}](${fileInfo.url})`;
-      }
-
-      await vexClient.users.nick(msg.author.username);
-      await vexClient.messages.send(
-        process.env.VEX_CHANNEL_ID!,
-        msg.content + (attachment ? "\n" + attachment : "")
-      );
-    }
-  }
-});
--- src/utils/loadEnv.ts
+++ src/utils/loadEnv.ts
@@ -5,14 +5,21 @@ import { config } from "dotenv";
   Thanks Z for this function.
 */
 export function loadEnv(): void {
-  config();
-  const requiredEnvVars: string[] = [];
-  for (const required of requiredEnvVars) {
-    if (process.env[required] === undefined) {
-      console.warn(
-        `Required environment variable '${required}' is not set. Please consult the README.`
-      );
-      process.exit(1);
+    config();
+    const requiredEnvVars: string[] = [
+        "VEX_CHANNEL_ID",
+        "PK",
+        "PW",
+        "DISCORD_TOKEN",
+        "DISCORD_SERVER_ID",
+        "DISCORD_CHANNEL_ID",
+    ];
+    for (const required of requiredEnvVars) {
+        if (process.env[required] === undefined) {
+            console.warn(
+                `Required environment variable '${required}' is not set. Please consult the README.`
+            );
+            process.exit(1);
+        }
     }
-  }
 }
